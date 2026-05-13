c900a5b72a13af2b433453b2420f798ecdbd8a8d
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2026-05-13 15:42:30 -0300

    Feat: Adding board.yml

--- boards/arm/eyelash_sofle/board.cmake
+++ boards/arm/eyelash_sofle/board.cmake
@@ -1,8 +1,6 @@
 # Copyright (c) 2021 @MangoIV
 # SPDX-License-Identifier: MIT
 
-set(SOC_SERIES nrf52)
-
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
